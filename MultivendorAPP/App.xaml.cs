2e1b8dc048dba061c45ff071d843fce2dec1469c
mohd nur syazwan <syazwan089@gmail.com>
2020-01-06 17:33:39 +0800

    Add project files.

---
+++ MultivendorAPP/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MultivendorAPP.Services;
+using MultivendorAPP.Views;
+using Xamarin.Essentials;
+
+namespace MultivendorAPP
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (!String.IsNullOrEmpty(Preferences.Get("token", "")))
+            {
+                MainPage = new AppShell();
+              
+            }
+
+            else
+            {
+                MainPage = new NavigationPage(new RegisterPage());
+            }
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MultivendorAPP/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MultivendorAPP.ViewModels
+{
+    public class RegisterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public RegisterViewModel()
+        {
+
+        }
+    }
+}
