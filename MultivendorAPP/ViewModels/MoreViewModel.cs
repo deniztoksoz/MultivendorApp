ec65be3e963f6e0636d2879f4a8c66a4b373dbda
mohd nur syazwan <syazwan089@gmail.com>
2020-01-09 16:29:11 +0800

    fix design

---
+++ MultivendorAPP/ViewModels/MoreViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MultivendorAPP.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MultivendorAPP.ViewModels
+{
+    public class MoreViewModel
+    {
+        public Command LogoutCommand { get; set; }
+        public MoreViewModel()
+        {
+            LogoutCommand = new Command(logout);
+        }
+
+        private async void logout()
+        {
+            var isSignOut = await Application.Current.MainPage.DisplayAlert("Are you sure", "You can always come back", "Yes", "Cancel");
+
+            if (isSignOut)
+            {
+                Preferences.Remove("token");
+
+                if (String.IsNullOrEmpty(Preferences.Get("token", "")))
+                {
+                    Application.Current.MainPage = new NavigationPage(new LoginPage());
+                }
+
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Error", "cannot sign out", "cancel");
+                }
+            }
+        }
+    }
+}
